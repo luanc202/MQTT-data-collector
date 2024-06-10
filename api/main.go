3fd3b018e05f53ec2d31e2ce04a0b15d2a2debdf
Luan A. <barkon32@tuta.io>
2024-06-10 20:11:25 -0300

    feat: add entry main file

---
+++ api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/luanc202/timeseriesSensorCollector/infra/config"
+	"github.com/luanc202/timeseriesSensorCollector/infra/db"
+	"github.com/luanc202/timeseriesSensorCollector/usecase"
+)
+
+func main() {
+	env, err := config.LoadEnv(".")
+	if err != nil {
+		panic(err)
+	}
+
+	
+	err = config.InitConfigs()
+	if err != nil {
+		panic(err)
+	}
+
+	mqttClient := config.GetMqttClient()
+	
+	if token := mqttClient.Connect(); token.Wait() && token.Error()!= nil {
+		panic(token.Error())
+	}
+
+	dbCollectorRepo := db.NewCollectorRepository(config.GetWriteAPI(), config.GetQueryAPI())
+
+	sensorUseCase := usecase.NewSensorUseCase(dbCollectorRepo)
+
+	token := mqttClient.Subscribe(env.MQTT_Topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Println(string(msg.Payload()))
+		sensorUseCase.Save(msg)
+	})
+	token.Wait()
+
+	fmt.Printf("running on port %v \n", env.PORT)
+	
+	select {}
+}
