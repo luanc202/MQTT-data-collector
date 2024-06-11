9565f1b200b21738b3114d12675d71d448c8ded7
Luan A. <barkon32@tuta.io>
2024-06-11 16:40:40 -0300

    feat: formatting files

--- usecase/sensor.go
+++ usecase/sensor.go
@@ -10,39 +10,34 @@ import (
 	"github.com/luanc202/timeseriesSensorCollector/interfaces"
 )
 
-
-
 var loggerUsecase = config.GetLogger("sensor-usecase")
 
 type SensorUsecase struct {
-  repo interfaces.CollectorRepository
+	repo interfaces.CollectorRepository
 }
 
 func NewSensorUseCase(repo interfaces.CollectorRepository) *SensorUsecase {
-  return &SensorUsecase{repo: repo}
+	return &SensorUsecase{repo: repo}
 }
 
-func (c *SensorUsecase) Save(msg mqtt.Message) (error) {
+func (c *SensorUsecase) Save(msg mqtt.Message) error {
 
-  var data dto.SensorDataDto
+	var data dto.SensorDataDto
 
-  err := json.Unmarshal(msg.Payload(), &data)
-  if err!= nil {
+	err := json.Unmarshal(msg.Payload(), &data)
+	if err != nil {
 		return fmt.Errorf("error decoding message payload: %v", err)
 	}
 
-  err = data.Validate()
-
-  if err!= nil {
+	err = data.Validate()
+	if err != nil {
 		return fmt.Errorf("error validating data dto: %v", err)
 	}
-
-  err = c.repo.Save(&data)
-
-  if err != nil {
-    loggerUsecase.Error("error saving data", err)
+	err = c.repo.Save(&data)
+	if err != nil {
+		loggerUsecase.Error("error saving data", err)
 		return fmt.Errorf("failed to save data: %w", err)
-  }
+	}
 
-  return nil
+	return nil
 }
